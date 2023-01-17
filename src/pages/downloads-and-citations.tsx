78f2f44d0f6ad84cdcf1150efa68e4e13ec075d2
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-16 21:01:20 -0300

    Adding placeholder pages for overview, downloads, and methods

---
+++ src/pages/downloads-and-citations.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import CMS from '@talus-analytics/library.airtable-cms'
+import Providers from '../components/layout/Providers'
+
+import Main from '../components/layout/Main'
+
+import NavBar from 'components/layout/NavBar/NavBar'
+import AboutNav from 'components/about/AboutNav'
+
+const DownloadsPage = (): JSX.Element => {
+  return (
+    <Providers>
+      <CMS.SEO />
+      <NavBar />
+      <Main>
+        <AboutNav />
+        <h1>Downloads</h1>
+      </Main>
+    </Providers>
+  )
+}
+
+export default DownloadsPage
---
+++ src/pages/overview.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import CMS from '@talus-analytics/library.airtable-cms'
+import Providers from '../components/layout/Providers'
+
+import Main from '../components/layout/Main'
+
+import NavBar from 'components/layout/NavBar/NavBar'
+import AboutNav from 'components/about/AboutNav'
+
+const OverviewPage = (): JSX.Element => {
+  return (
+    <Providers>
+      <CMS.SEO />
+      <NavBar />
+      <Main>
+        <AboutNav />
+        <h1>Overview</h1>
+      </Main>
+    </Providers>
+  )
+}
+
+export default OverviewPage
