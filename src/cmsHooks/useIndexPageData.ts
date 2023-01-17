271ebdf2b86142e48ee19a3f846783988690d8ae
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-17 14:44:49 -0300

    Making this example query an unnamed query to make copy/pasting it easier

--- src/cmsHooks/useIndexPageData.ts
+++ src/cmsHooks/useIndexPageData.ts
@@ -7,7 +7,7 @@ import { AirtableCMSData } from '@talus-analytics/library.airtable-cms'
 const useIndexPageData = () => {
   const { cmsContent }: { cmsContent: AirtableCMSData } =
     useStaticQuery(graphql`
-      query cmsContentQuery {
+      query {
         cmsContent: allAirtable(filter: { table: { eq: "Landing Page" } }) {
           nodes {
             data {
