32b074b12f561e1725e1601d110b34ec1d265edd
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-17 10:24:00 -0300

    Cleaning up imports

--- src/pages/about/methods.tsx
+++ src/pages/about/methods.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
 import CMS from '@talus-analytics/library.airtable-cms'
+
 import Providers from 'components/layout/Providers'
-
 import Main from 'components/layout/Main'
-
 import NavBar from 'components/layout/NavBar/NavBar'
 import AboutNav from 'components/about/AboutNav'
 
