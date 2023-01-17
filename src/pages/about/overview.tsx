bc5cfa9044a10fa0e2b670acbdeda123333c7147
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-17 14:49:25 -0300

    Adding CMS data query and calling H1 and Top Paragraph CMS data

--- src/pages/about/overview.tsx
+++ src/pages/about/overview.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import CMS from '@talus-analytics/library.airtable-cms'
 
-import Providers from 'components/layout/Providers'
 import Main from 'components/layout/Main'
 import NavBar from 'components/layout/NavBar/NavBar'
 import AboutNav from 'components/about/AboutNav'
+import Providers from 'components/layout/Providers'
+
+import useAboutOverviewPageData from 'cmsHooks/useAboutOverviewQuery'
 
 const OverviewPage = (): JSX.Element => {
+  const cmsData = useAboutOverviewPageData()
+
   return (
     <Providers>
       <CMS.SEO />
       <NavBar />
       <Main>
         <AboutNav />
-        <h1>Overview</h1>
+        <h1>
+          <CMS.Text name="H1" data={cmsData} />
+        </h1>
+        <p>
+          <CMS.Text name="Top paragraph" data={cmsData} />
+        </p>
       </Main>
     </Providers>
   )
