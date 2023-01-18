4cf26d97f8bdd77ab8f913e189ed4018e4b0a38c
David Rosado <drosado@talusanalytics.com>
2023-01-18 11:20:10 -0500

    overview content and styles

--- src/pages/about/overview.tsx
+++ src/pages/about/overview.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import CMS from '@talus-analytics/library.airtable-cms'
 
 import Main from 'components/layout/Main'
@@ -7,6 +9,18 @@ import AboutNav from 'components/about/AboutNav'
 import Providers from 'components/layout/Providers'
 
 import useAboutOverviewPageData from 'cmsHooks/useAboutOverviewQuery'
+
+const Paragraph = styled.p`
+  max-width: 1000px;
+`
+
+const FunderImage = styled(CMS.Image)`
+  width: 633px;
+`
+
+const GUImage = styled(CMS.Image)`
+  max-width: 735px;
+`
 
 const OverviewPage = (): JSX.Element => {
   const cmsData = useAboutOverviewPageData()
@@ -20,9 +34,23 @@ const OverviewPage = (): JSX.Element => {
         <h1>
           <CMS.Text name="H1" data={cmsData} />
         </h1>
-        <p>
+        <Paragraph>
           <CMS.Text name="Top paragraph" data={cmsData} />
-        </p>
+        </Paragraph>
+        <h1>
+          <CMS.Text name="H2" data={cmsData} />
+        </h1>
+        <FunderImage name="Funder icon" data={cmsData} />
+        <Paragraph>
+          <CMS.Text name="Funder paragraph" data={cmsData} />
+        </Paragraph>
+        <h1>
+          <CMS.Text name="H2 contributors" data={cmsData} />
+        </h1>
+        <GUImage name="Contributors icon" data={cmsData} />
+        <Paragraph>
+          <CMS.Text name="Contributors paragraph" data={cmsData} />
+        </Paragraph>
       </Main>
     </Providers>
   )
