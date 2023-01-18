b3ab00e17a76a441504b91322ba9a463f4495ef4
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-18 17:42:13 -0300

    Adding links to topics in the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Main from '../components/layout/Main'
 
 import useIndexPageData from '../cmsHooks/useIndexPageData'
 import NavBar from 'components/layout/NavBar/NavBar'
+import { Link } from 'gatsby'
 
 const Header = styled.header`
   text-align: center;
@@ -15,8 +16,8 @@ const Header = styled.header`
   margin: auto;
 `
 const IntroParagraph = styled.p`
-  color: ${({ theme }) => theme.veryDarkGray};
   ${({ theme }) => theme.bigParagraph};
+  color: ${({ theme }) => theme.veryDarkGray};
 `
 const H1 = styled.h1`
   color: ${({ theme }) => theme.ampEidGreen};
@@ -49,10 +50,43 @@ const H4 = styled.h4`
 `
 const TopicLinkList = styled.ul`
   list-style: none;
+  display: flex;
+  gap: 30px;
+`
+const Li = styled.li`
+  flex-basis: 30%;
+  display: block;
+  border: 1px solid ${({ theme }) => theme.black};
+  border-radius: 5px;
+`
+const ImageLink = styled(Link)`
+  display: block;
+  position: relative;
+  background: white;
+  transition: 150ms ease;
+
+  &:hover {
+    background: ${({ theme }) => theme.lightGray};
+  }
+`
+const ComingSoonLinkPlaceholder = styled.span`
+  display: block;
+  position: relative;
+`
+const ImageLinkTextContainer = styled.div`
+  ${({ theme }) => theme.bigParagraph};
+  position: absolute;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: ${({ theme }) => theme.black};
+  background: rgba(255, 255, 255, 0.85);
+  border-radius: 5px;
 `
 
 const IndexPage = (): JSX.Element => {
-  const data = useIndexPageData()
+  const cmsData = useIndexPageData()
 
   return (
     <Providers>
@@ -61,22 +95,48 @@ const IndexPage = (): JSX.Element => {
       <Main>
         <Header>
           <H2>
-            <CMS.Text name="H2" data={data} />
+            <CMS.Text name="H2" data={cmsData} />
           </H2>
           <H1>
-            <CMS.Text name="H1" data={data} />
+            <CMS.Text name="H1" data={cmsData} />
           </H1>
           <IntroParagraph>
-            <CMS.Text name="Intro paragraph" data={data} />
+            <CMS.Text name="Intro paragraph" data={cmsData} />
           </IntroParagraph>
         </Header>
         <TopicSection>
           <H3>
-            <CMS.Text name="H3" data={data} />
+            <CMS.Text name="H3" data={cmsData} />
           </H3>
           <H4>
-            <CMS.Text name="H4" data={data} />
+            <CMS.Text name="H4" data={cmsData} />
           </H4>
+          <TopicLinkList>
+            <Li>
+              <ImageLink to="/topics/trips/">
+                <CMS.Image name="TRIPS image" data={cmsData} />
+                <ImageLinkTextContainer>
+                  <CMS.Text name="TRIPS text" data={cmsData} />
+                </ImageLinkTextContainer>
+              </ImageLink>
+            </Li>
+            <Li>
+              <ComingSoonLinkPlaceholder>
+                <CMS.Image name="Childhood vaccination image" data={cmsData} />
+                <ImageLinkTextContainer>
+                  <CMS.Text name="Childhood vaccination text" data={cmsData} />
+                </ImageLinkTextContainer>
+              </ComingSoonLinkPlaceholder>
+            </Li>
+            <Li>
+              <ComingSoonLinkPlaceholder>
+                <CMS.Image name="Non-human vaccination image" data={cmsData} />
+                <ImageLinkTextContainer>
+                  <CMS.Text name="Non-human vaccination text" data={cmsData} />
+                </ImageLinkTextContainer>
+              </ComingSoonLinkPlaceholder>
+            </Li>
+          </TopicLinkList>
         </TopicSection>
       </Main>
     </Providers>
