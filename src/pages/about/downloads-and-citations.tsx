0e1f02ccadc46d8bbf384f20ace66144ed593215
David Rosado <drosado@talusanalytics.com>
2023-01-18 16:01:45 -0500

    download and citations page

--- src/pages/about/downloads-and-citations.tsx
+++ src/pages/about/downloads-and-citations.tsx
@@ -5,15 +5,67 @@ import Providers from 'components/layout/Providers'
 import Main from 'components/layout/Main'
 import NavBar from 'components/layout/NavBar/NavBar'
 import AboutNav from 'components/about/AboutNav'
+import styled from 'styled-components'
+
+import useAboutDownloadAndCitationsPageData from 'cmsHooks/useAboutDownloadAndCitationsQuery'
+
+const Paragraph = styled.p`
+  color: ${({ theme }) => theme.black};
+  ${({ theme }) => theme.bigParagraph};
+  max-width: 1000px;
+`
+const ItalicParagraph = styled.p`
+  color: ${({ theme }) => theme.black};
+  font-family: 'Poppins';
+  font-style: italic;
+  max-width: 970px;
+  font-size: 20px;
+  padding: 15px;
+`
+
+const H1 = styled.h1`
+  ${({ theme }) => theme.h1}
+  color: ${({ theme }) => theme.black};
+`
+
+const H2 = styled.h2`
+  ${({ theme }) => theme.h2}
+  color: ${({ theme }) => theme.black};
+`
 
 const DownloadsPage = (): JSX.Element => {
+  const cmsData = useAboutDownloadAndCitationsPageData()
+
   return (
     <Providers>
       <CMS.SEO />
       <NavBar />
       <Main>
         <AboutNav />
-        <h1>Downloads</h1>
+        <H1>
+          <CMS.Text name="H1" data={cmsData} />
+        </H1>
+        <Paragraph>
+          <CMS.Text name="Top paragraph" data={cmsData} />
+        </Paragraph>
+        <H2>
+          <CMS.Text name="H2" data={cmsData} />
+        </H2>
+        <Paragraph>
+          <CMS.Text name="Bottom paragraph" data={cmsData} />
+        </Paragraph>
+        <ItalicParagraph>
+          <CMS.Text name="CGHSS paragraph" data={cmsData} />
+        </ItalicParagraph>
+        <Paragraph>
+          <CMS.Text name="Contact paragraph" data={cmsData} />
+        </Paragraph>
+        <Paragraph>
+          <CMS.Text name="Tools paragraph" data={cmsData} />
+        </Paragraph>
+        <Paragraph>
+          <CMS.Text name="Downloads paragraph" data={cmsData} />
+        </Paragraph>
       </Main>
     </Providers>
   )
