025d5554ad701e28898f9078bdb808fd1f790d1d
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-20 14:48:16 -0300

    Adding type guards

--- src/utilities/camelCase.ts
+++ src/utilities/camelCase.ts
@@ -1,7 +1,12 @@
 // Adapted from
 // https://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
 
-const camelCase = (str: string) =>
-  str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase())
+const camelCase = (str: string | undefined | null) => {
+  if (str === undefined) return undefined
+  if (str === null) return null
+  return str
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (_, chr) => chr.toUpperCase())
+}
 
 export default camelCase
