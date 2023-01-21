463b51f87e987ddedf437d121f04dde7b5cededf
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-21 17:03:53 -0300

    Adding basic layout of title and treaty description paragraph

--- src/templates/treaty.tsx
+++ src/templates/treaty.tsx
@@ -1,20 +1,62 @@
-import { graphql, PageProps } from 'gatsby'
 import React from 'react'
+import styled from 'styled-components'
+import { graphql, PageProps } from 'gatsby'
 import CMS from '@talus-analytics/library.airtable-cms'
+import { RenderCMSRichText } from '@talus-analytics/library.airtable.cms-rich-text'
 
 import Main from 'components/layout/Main'
 import Providers from 'components/layout/Providers'
 import MainHeader from 'components/layout/MainHeader'
+import NavBar from 'components/layout/NavBar/NavBar'
+
+const Layout = styled.div`
+  margin-top: 30px;
+  display: flex;
+  gap: 50px;
+`
+const Thumbnail = styled.div`
+  width: 350px;
+  background-color: lightgray;
+  flex-shrink: 0;
+`
+const MainContent = styled.div``
+const H3 = styled.h3`
+  margin-top: 0;
+  margin-bottom: 20px;
+  ${({ theme }) => theme.h3}
+  color: ${({ theme }) => theme.black};
+`
+const Description = styled(RenderCMSRichText)`
+  > p {
+    margin: 0;
+    ${({ theme }) => theme.paragraph}
+    color: ${({ theme }) => theme.black};
+    > a {
+      color: ${({ theme }) => theme.ampEidMedBlue};
+      text-decoration: underline;
+    }
+  }
+`
 
 const TreatyPage = ({ data }: PageProps<Queries.TreatyPageQuery>) => {
   const treatyData = data.general.nodes[0]
+
   return (
     <Providers>
       <CMS.SEO />
+      <NavBar />
       <Main>
         <MainHeader>
+          <h2>TREATY</h2>
           <h1>{treatyData.data?.Document_name}</h1>
         </MainHeader>
+        <Layout>
+          <Thumbnail />
+          <MainContent>
+            <H3>Description</H3>
+            <Description markdown={treatyData.data?.Treaty_description ?? ''} />
+          </MainContent>
+        </Layout>
       </Main>
     </Providers>
   )
