33718d86c2c088421912dde17cf8a54332c77022
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-21 16:39:44 -0300

    Adding short names query and createPage calls to create treaty pages

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,1 +1,39 @@
 // use Typescript with gatsby node apis here
+import * as path from 'path'
+
+import { GatsbyNode } from 'gatsby'
+
+export const createPages: GatsbyNode['createPages'] = async ({
+  actions,
+  graphql,
+}) => {
+  const treatyPageTemplate = path.resolve('./src/templates/treaty.tsx')
+
+  const result = await graphql<Queries.TreatyShortNamesQuery>(`
+    query ShortNames {
+      shortNames: allAirtableTreaties(
+        filter: {
+          table: {
+            eq: "ALL: ONLY USE THIS TAB INSTEAD OF THE SINGLE TABS FROM NOW ON"
+          }
+        }
+      ) {
+        distinct(
+          field: {
+            data: { Treaty_name: { data: { Treaty_short_name: SELECT } } }
+          }
+        )
+      }
+    }
+  `)
+
+  if (!result.data?.shortNames) throw new Error('No treaties found')
+
+  for (const short_name of result.data.shortNames.distinct) {
+    actions.createPage({
+      path: `/treaties/${short_name}/`,
+      component: treatyPageTemplate,
+      context: { short_name },
+    })
+  }
+}
