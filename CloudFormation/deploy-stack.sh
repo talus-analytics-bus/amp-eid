a179d0621c0b522e4dfd92ce5533f280c499db08
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-23 15:07:41 -0300

    Correcting the deploy-stack and update-stack scripts

--- CloudFormation/deploy-stack.sh
+++ CloudFormation/deploy-stack.sh
@@ -2,5 +2,5 @@ aws cloudformation deploy \
   --stack-name amp-eid \
   --template-file build-stack.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --tags Key=Project,value=IDEA \
-         Key=ProjectDetail,value=AMPEID \
+  --tags Project=IDEA \
+         Project:Detail=AMPEID \
--- CloudFormation/update-stack.sh
+++ CloudFormation/update-stack.sh
@@ -2,5 +2,5 @@ aws cloudformation update-stack \
   --stack-name amp-eid \
   --template-body file://build-stack.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --tags Key=Project,value=IDEA \
-         Key=ProjectDetail,value=AMPEID \
+  --tags Key=Project,Value=IDEA \
+         Key=Project:Detail,Value=AMPEID \
