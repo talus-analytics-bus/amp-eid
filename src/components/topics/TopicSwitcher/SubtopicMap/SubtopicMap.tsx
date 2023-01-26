130e11b535c3d7e32aa8480cdac8139ac3b7cb3f
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-26 15:13:15 -0300

    Adding map component with proof of concept country highlighting from data & figma colors

--- src/components/topics/TopicSwitcher/SubtopicMap/SubtopicMap.tsx
+++ src/components/topics/TopicSwitcher/SubtopicMap/SubtopicMap.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import styled from 'styled-components'
+import React, { useContext, useEffect, useRef } from 'react'
+import styled, { useTheme } from 'styled-components'
 import { SubtopicContext } from '../TopicSwitcher'
 
 import mapboxgl from 'mapbox-gl'
@@ -16,34 +16,120 @@ if (!mapboxKey)
 
 mapboxgl.accessToken = mapboxKey
 
-const MapPlaceholder = styled.div`
+const MapSection = styled.section`
   position: relative;
   flex-grow: 1;
-  height: 50vh;
-  background-color: ${({ theme }) => theme.lightGray};
   display: flex;
-  align-items: center;
+  height: 32vw;
+  flex-direction: column;
+  align-items: flex-start;
   justify-content: center;
+  background: ${({ theme }) => theme.veryLightGray};
+`
+const MapContainer = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
 `
 const MapTitle = styled.h3`
   ${({ theme }) => theme.h3};
   color: ${({ theme }) => theme.black};
-  position: absolute;
   margin: 0;
-  padding: 0;
-  top: 15px;
-  left: 30px;
+  padding: 10px 20px;
 `
 
 const SubtopicMap = () => {
+  const theme = useTheme()
   const context = useContext(SubtopicContext)
-  if (!context) throw new Error('MapLegend must be inside SubtopicContext')
+  if (!context) throw new Error('SubtopicMap must be inside SubtopicContext')
   const { subtopicIndex, subtopicData } = context
 
+  const mapContainer = useRef<HTMLDivElement>(null)
+  const mapRef = useRef<null | mapboxgl.Map>(null)
+
+  useEffect(() => {
+    // don't mount map if ref is null or map is already mounted
+    let map = mapRef.current
+    if (!mapContainer.current || map) return
+
+    map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/ryan-talus/clddahzv7007j01qbgn0bba8w',
+      // style: { version: 8, sources: {}, layers: [] },
+      projection: { name: 'naturalEarth' },
+      maxZoom: 5,
+      minZoom: 0,
+      maxBounds: [
+        [-180, -80],
+        [180, 80],
+      ],
+    })
+
+    map.fitBounds(
+      [
+        [160, -60],
+        [180, 90],
+      ],
+      { padding: 30 }
+    )
+
+    const highlight = ['USA', 'ARG', 'MEX']
+
+    map.on('load', () => {
+      map.addSource('countries', {
+        type: 'vector',
+        url: 'mapbox://ryan-talus.0h741z23',
+      })
+      map.addLayer(
+        {
+          id: 'countries-fill',
+          type: 'fill',
+          source: 'countries',
+          'source-layer': 'ne_10m_admin_0_countries-6llcvl',
+          paint: {
+            'fill-outline-color': 'white',
+            'fill-color': theme.option6,
+          },
+        },
+        'country-label'
+      )
+
+      map.addLayer(
+        {
+          id: 'countries-highlight',
+          type: 'fill',
+          source: 'countries',
+          'source-layer': 'ne_10m_admin_0_countries-6llcvl',
+          paint: {
+            'fill-outline-color': 'white',
+            'fill-color': theme.option1,
+          },
+          filter: ['in', 'ADM0_ISO', ...highlight],
+        },
+        'country-label'
+      )
+
+      map.addLayer(
+        {
+          id: 'countries-stroke',
+          type: 'line',
+          source: 'countries',
+          'source-layer': 'ne_10m_admin_0_countries-6llcvl',
+          paint: {
+            'line-color': 'white',
+            'line-width': 0.5,
+          },
+        },
+        'country-label'
+      )
+    })
+  }, [theme])
+
   return (
-    <MapPlaceholder>
+    <MapSection>
       <MapTitle>{subtopicData[subtopicIndex ?? 0].data?.Subtopic}</MapTitle>
-    </MapPlaceholder>
+      <MapContainer ref={mapContainer} />
+    </MapSection>
   )
 }
 
