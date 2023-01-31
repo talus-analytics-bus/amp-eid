89585b90da6c513e9d25c686c1047f1785bde63f
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-31 17:16:37 -0300

    adding formatting for basic document page information

--- src/templates/document.tsx
+++ src/templates/document.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react'
 import { graphql, PageProps } from 'gatsby'
+import Providers from 'components/layout/Providers'
+import CMS from '@talus-analytics/library.airtable-cms'
+import NavBar from 'components/layout/NavBar/NavBar'
+import Main from 'components/layout/Main'
+import MainHeader from 'components/layout/MainHeader'
 
 const DocumentPage = ({
   data: { document },
 }: PageProps<Queries.DocumentPageQuery>) => {
-  console.log(document)
-  return <h1>{document?.data?.Document_name}</h1>
+  if (!document?.data || !document.data.Document_name)
+    throw new Error('All documents must have a unique document name.')
+  return (
+    <Providers>
+      <CMS.SEO
+        title={document.data.Document_name}
+        description={
+          `Applicable countries, metadata, and download ` +
+          `links for ${document.data.Document_name}`
+        }
+      />
+      <NavBar />
+      <Main>
+        <MainHeader>
+          <h2>{document.data.Topic}</h2>
+          <h1>{document.data.Document_name}</h1>
+        </MainHeader>
+      </Main>
+    </Providers>
+  )
 }
 
 export const query = graphql`
