92f1cb56d6bfe7cd2a1aaddb22ac95315ddec580
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-01 19:28:26 -0300

    Expanding options and making column section force the right side to be wide

--- src/components/layout/ColumnSection.tsx
+++ src/components/layout/ColumnSection.tsx
@@ -1,27 +1,36 @@
 import styled from 'styled-components'
 
-const ColumnSection = styled.section`
+const ColumnSection = styled.section<{
+  noBorder?: boolean
+  columnReverse?: boolean
+}>`
   margin-top: 20px;
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
   gap: 30px;
 
-  padding-top: 30px;
-  border-top: 3px solid ${({ theme }) => theme.lightGray};
+  padding-top: 20px;
 
   @media (max-width: 1000px) {
-    flex-direction: column;
+    flex-direction: ${({ columnReverse }) =>
+      columnReverse ? 'column-reverse' : 'column'};
   }
 
   & > :nth-child(1) {
-    min-width: 350px;
+    display: block;
+    width: 350px;
+    flex-shrink: 0;
+
     @media (max-width: 1000px) {
       min-width: auto;
+      width: auto;
     }
   }
 
   & > :nth-child(2) {
+    display: block;
+    min-width: 350px;
     flex-grow: 1;
     display: flex;
     flex-direction: column;
@@ -29,6 +38,9 @@ const ColumnSection = styled.section`
     justify-content: center;
     gap: 20px;
   }
+
+  ${({ theme, noBorder }) =>
+    !noBorder && `border-top: 3px solid ${theme.lightGray};`};
 `
 
 export default ColumnSection
