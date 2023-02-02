c5043847e75adc78df6233a7ea2af139291a25b9
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-02 19:11:17 -0300

    Expanding explore policies to show thumbnails and have styling

--- src/components/topics/ExplorePolicies/ExplorePolicies.tsx
+++ src/components/topics/ExplorePolicies/ExplorePolicies.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import styled from 'styled-components'
-import { Link } from 'gatsby'
 
 import Dropdown from '@talus-analytics/library.ui.dropdown'
 
 import ColumnSection from 'components/layout/ColumnSection'
 import PaginationControls from './PaginationControls'
-import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+import DocumentLink from './DocumentLink'
 
 const H3 = styled.h3`
   ${({ theme }) => theme.h2};
@@ -25,14 +25,39 @@ const DropdownButton = styled.button`
   align-items: center;
   gap: 15px;
 `
+const DropdownContent = styled.div`
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
+  background: ${({ theme }) => theme.veryLightGray};
+`
 
 interface ExplorePoliciesProps {
   countryDocuments: Queries.TripsPageQuery['countryDocuments']
+  thumbnails: Queries.TripsPageQuery['thumbnails']
 }
 
-const ExplorePolicies = ({ countryDocuments }: ExplorePoliciesProps) => {
+export interface ThumbnailMap {
+  [key: string]: IGatsbyImageData
+}
+
+const ExplorePolicies = ({
+  countryDocuments,
+  thumbnails,
+}: ExplorePoliciesProps) => {
   const [page, setPage] = useState(0)
   const [pageSize, setPageSize] = useState(5)
+
+  // map between document name and thumbnail data
+  const thumbnailMap: ThumbnailMap = useMemo(() => {
+    return thumbnails.nodes.reduce(
+      (obj, doc) => ({
+        ...obj,
+        [doc.data!.Document_name!]:
+          doc.documentThumbnail?.[0]?.childImageSharp?.gatsbyImageData,
+      }),
+      {}
+    )
+  }, [thumbnails])
 
   // removing 'readonly' from the gatsby-generated type
   // so we can sort without typescript complaining
@@ -44,7 +69,6 @@ const ExplorePolicies = ({ countryDocuments }: ExplorePoliciesProps) => {
     (a, b) =>
       a.data?.Country_name?.localeCompare(b.data?.Country_name ?? '') ?? -1
   )
-
   const total = sorted.length
   const paginated = sorted.slice(page * pageSize, page * pageSize + pageSize)
 
@@ -53,30 +77,32 @@ const ExplorePolicies = ({ countryDocuments }: ExplorePoliciesProps) => {
       <H3>Explore Policies</H3>
       <div>
         {paginated.map(country => (
-          <Dropdown
-            key={country.data?.ISO_3166_1_alpha_3}
-            floating={false}
-            renderButton={() => (
-              <DropdownButton>
-                {country.flag?.childImageSharp?.gatsbyImageData && (
-                  <GatsbyImage
-                    image={country.flag.childImageSharp.gatsbyImageData}
-                    alt={`${country.data?.Country_name} Flag`}
-                  />
+          <React.Fragment key={country.data?.ISO_3166_1_alpha_3}>
+            {country.data?.ISO_3166_1_alpha_3 && (
+              <Dropdown
+                floating={false}
+                renderButton={() => (
+                  <DropdownButton>
+                    {country.flag?.childImageSharp?.gatsbyImageData && (
+                      <GatsbyImage
+                        image={country.flag.childImageSharp.gatsbyImageData}
+                        alt={`${country.data?.Country_name} Flag`}
+                      />
+                    )}
+                    {country.data?.Country_name}
+                  </DropdownButton>
                 )}
-                {country.data?.Country_name}
-              </DropdownButton>
-            )}
-          >
-            {country.data?.All_applicable_countries_link?.map(document => (
-              <Link
-                key={document?.data?.Document_name}
-                to={`/documents/${document?.data?.Document_name}`}
               >
-                {document?.data?.Document_name}
-              </Link>
-            ))}
-          </Dropdown>
+                <DropdownContent>
+                  {country.data?.All_applicable_countries_link?.map(
+                    document => (
+                      <DocumentLink {...{ document, thumbnailMap }} />
+                    )
+                  )}
+                </DropdownContent>
+              </Dropdown>
+            )}
+          </React.Fragment>
         ))}
         <PaginationControls
           {...{ page, setPage, pageSize, setPageSize, total }}
