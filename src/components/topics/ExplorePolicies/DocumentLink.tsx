8ffe41fa0be0177435caf8136f6ce910b95d44f2
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-02 19:10:50 -0300

    Component that takes a document and thumbnail map and renders a link

---
+++ src/components/topics/ExplorePolicies/DocumentLink.tsx
@@ -0,0 +1,55 @@
+import { Link } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import React from 'react'
+import styled from 'styled-components'
+
+import { ThumbnailMap } from './ExplorePolicies'
+
+interface DocumentLinkProps {
+  document?: {
+    data?: {
+      Document_name?: string | null
+    } | null
+  } | null
+  thumbnailMap: ThumbnailMap
+}
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
+  margin-left: 75px;
+  margin-right: 75px;
+  border-top: 1px solid ${({ theme }) => theme.medGray};
+  text-decoration: none;
+`
+const Thumbnail = styled(GatsbyImage)`
+  flex-shrink: 0;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+`
+const Metadata = styled.div`
+  padding: 0 15px;
+`
+const Name = styled.div`
+  ${({ theme }) => theme.h3};
+  color: ${({ theme }) => theme.black};
+`
+
+const DocumentLink = ({ document, thumbnailMap }: DocumentLinkProps) => {
+  const name = document?.data?.Document_name
+  if (!name) throw new Error('Document missing name')
+  const image = thumbnailMap[name]
+
+  console.log(document)
+
+  return (
+    <StyledLink key={name} to={`/documents/${name}`}>
+      {image && <Thumbnail image={image} alt={`${name} thumbnail`} />}
+      <Metadata>
+        <Name>{name}</Name>
+      </Metadata>
+    </StyledLink>
+  )
+}
+
+export default DocumentLink
