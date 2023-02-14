5621b65b0f22abf3cdfb1e946965ece23dd24e6e
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-14 20:09:08 +0000

    Filtering country names to not include "regional" and "Treaty"

--- src/queryHooks/useCountryNames.ts
+++ src/queryHooks/useCountryNames.ts
@@ -1,20 +1,24 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 const useCountryNames = () => {
-  const { countries } = useStaticQuery<Queries.CountryNamesQuery>(graphql`
-    query CountryNames {
-      countries: allAirtableDocuments(
-        filter: { table: { eq: "LOOKUP: Country" } }
-      ) {
-        nodes {
-          data {
-            Country_name
-            ISO3
+  const { countries } =
+    useStaticQuery<Queries.CountryNamesForSearchQuery>(graphql`
+      query CountryNamesForSearch {
+        countries: allAirtableDocuments(
+          filter: {
+            table: { eq: "LOOKUP: Country" }
+            data: { Country_name: { nin: ["Regional", "Treaty"] } }
+          }
+        ) {
+          nodes {
+            data {
+              Country_name
+              ISO3
+            }
           }
         }
       }
-    }
-  `)
+    `)
 
   return countries
 }
