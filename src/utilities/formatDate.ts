a318f9bff0e50a7eed958e97c23d17d62da19919
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-17 15:28:22 -0300

    Changing date format to match design

--- src/utilities/formatDate.ts
+++ src/utilities/formatDate.ts
@@ -1,13 +1,26 @@
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+
 const formatDate = (input: string | Date) => {
   if (typeof input === 'string') {
     input = new Date(input)
   }
 
-  return input.toLocaleString('en-US', {
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric',
-  })
+  return `${input.getDay()} ${
+    monthNames[input.getMonth()]
+  } ${input.getFullYear()}`
 }
 
 export default formatDate
