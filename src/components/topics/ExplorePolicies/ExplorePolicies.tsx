55713fe5b15e7b0da56092798ea95716ef51a974
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-20 11:23:13 -0300

    Refactoring ExplorePolicies to use the ExploreDropdown ui component

--- src/components/topics/ExplorePolicies/ExplorePolicies.tsx
+++ src/components/topics/ExplorePolicies/ExplorePolicies.tsx
@@ -2,36 +2,17 @@ import React, { useMemo, useState } from 'react'
 import styled from 'styled-components'
 import Fuse from 'fuse.js'
 
-import Dropdown from '@talus-analytics/library.ui.dropdown'
-
 import ColumnSection from 'components/layout/ColumnSection'
 import PaginationControls from './PaginationControls'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
 import DocumentLink from './DocumentLink'
 import Flag from 'components/ui/Flag'
+import ExploreDropdown from 'components/ui/ExploreDropdown'
 
 const H3 = styled.h3`
   ${({ theme }) => theme.h2};
   color: ${({ theme }) => theme.black};
   margin: 0;
-`
-const DropdownButton = styled.button`
-  ${({ theme }) => theme.bigParagraphMedium};
-  border: none;
-  background: ${({ theme }) => theme.veryLightGray};
-  color: ${({ theme }) => theme.black};
-  margin: none;
-  width: 100%;
-  padding: 10px 20px;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  gap: 15px;
-`
-const DropdownContent = styled.div`
-  border-bottom-right-radius: 5px;
-  border-bottom-left-radius: 5px;
-  background: ${({ theme }) => theme.veryLightGray};
 `
 const Label = styled.label`
   ${({ theme }) => theme.smallParagraph};
@@ -121,26 +102,21 @@ const ExplorePolicies = ({
         {paginated.map(country => (
           <React.Fragment key={country.data?.ISO3}>
             {country.data?.ISO3 && (
-              <Dropdown
-                floating={false}
-                renderButton={() => (
-                  <DropdownButton>
+              <ExploreDropdown
+                label={
+                  <>
                     <Flag country={country} />
                     {country.data?.Country_name}
-                  </DropdownButton>
-                )}
+                  </>
+                }
               >
-                <DropdownContent>
-                  {country.data?.All_applicable_countries_link?.map(
-                    document => (
-                      <DocumentLink
-                        key={document?.data?.Document_name}
-                        {...{ document, thumbnailMap }}
-                      />
-                    )
-                  )}
-                </DropdownContent>
-              </Dropdown>
+                {country.data?.All_applicable_countries_link?.map(document => (
+                  <DocumentLink
+                    key={document?.data?.Document_name}
+                    {...{ document, thumbnailMap }}
+                  />
+                ))}
+              </ExploreDropdown>
             )}
           </React.Fragment>
         ))}
