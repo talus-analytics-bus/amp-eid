7fcc32c05b414c761a4e28d2526536b10b62bb19
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-02-28 18:57:30 +0000

    Creating new treatyShortNames query

--- src/queryHooks/useShortTreatyNames.ts
+++ src/queryHooks/useShortTreatyNames.ts
@@ -1,22 +1,16 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 const useShortTreatyNames = () => {
-  return {}
-  // const { shortNames } = useStaticQuery<Queries.TreatyShortNamesQuery>(graphql`
-  //   query TreatyShortNames {
-  //     shortNames: allAirtableTreaties(
-  //       filter: { table: { eq: "All treaties and countries" } }
-  //     ) {
-  //       distinct(
-  //         field: {
-  //           data: { Treaty_name: { data: { Treaty_short_name: SELECT } } }
-  //         }
-  //       )
-  //     }
-  //   }
-  // `)
-
-  // return shortNames
+  const { shortNames } = useStaticQuery<Queries.TreatyShortNamesQuery>(graphql`
+    query TreatyShortNames {
+      shortNames: allAirtableDatabase(
+        filter: { table: { eq: "Document library" } }
+      ) {
+        distinct(field: { data: { Treaty_short_name: SELECT } })
+      }
+    }
+  `)
+  return shortNames
 }
 
 export default useShortTreatyNames
