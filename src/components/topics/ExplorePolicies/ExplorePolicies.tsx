bab5aa22d3ca246be86cfeeccb709d09e7ed5ed0
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-03-01 17:50:56 +0000

    Refactoring policy explore to work with new object structure

--- src/components/topics/ExplorePolicies/ExplorePolicies.tsx
+++ src/components/topics/ExplorePolicies/ExplorePolicies.tsx
@@ -9,6 +9,7 @@ import DocumentLink from 'components/ui/DocumentLink'
 import ExploreDropdown from 'components/ui/ExploreDropdown'
 
 import PaginationControls from './PaginationControls'
+import restructureDocuments from './restructureDocuments'
 
 const H3 = styled.h3`
   ${({ theme }) => theme.h2};
@@ -39,85 +40,20 @@ const ExplorePolicies = ({ topicDocuments }: ExplorePoliciesProps) => {
   const [pageSize, setPageSize] = useState(5)
   const [searchTerm, setSearchTerm] = useState('')
 
-  // // map between document name and thumbnail data
-  // const thumbnailMap = useMemo(() => {
-  //   return thumbnails.nodes.reduce(
-  //     (obj, doc) => ({
-  //       ...obj,
-  //       [doc.data!.Document_name!]: doc.documentThumbnail?.[0],
-  //     }),
-  //     {}
-  //   )
-  // }, [thumbnails])
-
-  console.time('countryDocuments')
   const countryDocuments = useMemo(() => {
-    // prettier-ignore
-    interface CountriesObj {
-      [key: string]: {
-        country: 
-          Exclude<
-            Exclude<
-              Exclude<
-                typeof topicDocuments.nodes[number], 
-                null>['data'], 
-              null
-            >['All_applicable_countries'],
-            null
-          >[number]
-
-        documents: Exclude<typeof topicDocuments.nodes[number][], null>
-      }
-    }
-    const countriesObj: CountriesObj = {}
-
-    for (const document of topicDocuments.nodes) {
-      const countries = document.data?.All_applicable_countries
-      if (!countries) break
-
-      for (const country of countries) {
-        if (!country?.data?.Country_name) break
-
-        if (!countriesObj[country.data.Country_name])
-          countriesObj[country.data.Country_name] = {
-            country,
-            documents: [document],
-          }
-        else countriesObj[country.data.Country_name].documents.push(document)
-      }
-    }
-    const countriesList = Object.entries(countriesObj).sort((a, b) =>
-      a[0].localeCompare(b[0])
-    )
-    return countriesList
+    return restructureDocuments(topicDocuments)
   }, [topicDocuments])
-  console.timeEnd('countryDocuments')
 
-  console.log(countryDocuments)
+  let displayCountries = countryDocuments
 
-  return <pre>{JSON.stringify(countryDocuments, null, 2)}</pre>
-
-  // removing 'readonly' from the gatsby-generated type
-  // so we can sort without typescript complaining
-  type countriesMutable = {
-    -readonly [key in keyof typeof countryDocuments.nodes[0]]: typeof countryDocuments.nodes[0][key]
-  }
-  const countries = countryDocuments.nodes as countriesMutable[]
-
-  const fuse = new Fuse(countries, {
+  const fuse = new Fuse(countryDocuments, {
     keys: ['data.Country_name', 'data.ISO3'],
   })
 
-  let sorted: typeof countries
+  if (searchTerm !== '')
+    displayCountries = fuse.search(searchTerm).map(result => result.item)
 
-  if (searchTerm === '')
-    sorted = countries.sort(
-      (a, b) =>
-        a.data?.Country_name?.localeCompare(b.data?.Country_name ?? '') ?? -1
-    )
-  else sorted = fuse.search(searchTerm).map(result => result.item)
-
-  const total = sorted.length
+  const total = displayCountries.length
 
   return (
     <ColumnSection>
@@ -137,9 +73,9 @@ const ExplorePolicies = ({ topicDocuments }: ExplorePoliciesProps) => {
         </Label>
       </div>
       <div>
-        {sorted.map((country, index) => (
-          <React.Fragment key={country.data?.Country_name}>
-            {country.data?.Country_name && (
+        {displayCountries.map(([name, data], index) => (
+          <React.Fragment key={name}>
+            {data?.country?.data?.Country_name && (
               <ExploreDropdown
                 style={{
                   display:
@@ -150,22 +86,17 @@ const ExplorePolicies = ({ topicDocuments }: ExplorePoliciesProps) => {
                 }}
                 label={
                   <>
-                    <Flag country={country} />
-                    {country.data?.Country_name}
+                    <Flag country={data.country} />
+                    {name}
                   </>
                 }
               >
-                {country.data.All_applicable_countries_link &&
-                country.data.All_applicable_countries_link.length > 0 ? (
-                  country.data?.All_applicable_countries_link?.map(document => (
-                    <DocumentLink
-                      key={document?.data?.Document_name}
-                      document={document}
-                    />
-                  ))
-                ) : (
-                  <p>No document</p>
-                )}
+                {data.documents.map(document => (
+                  <DocumentLink
+                    key={document?.data?.Document_name}
+                    document={document}
+                  />
+                ))}
               </ExploreDropdown>
             )}
           </React.Fragment>
