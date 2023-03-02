7d6d74ecc1bfd61b2faf7ef16a9eead1d3c40202
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-03-02 11:52:25 -0300

    Changing text styles and adding overpass font

--- src/figma/textStyles.ts
+++ src/figma/textStyles.ts
@@ -1,102 +1,110 @@
 export default {
   h1: `
-	font-size:36px;
-	font-family:"Poppins";
-	font-weight:500;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 36px;
+	font-family: "Overpass";
+	font-weight: 500;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   h2: `
-	font-size:30px;
-	font-family:"Poppins";
-	font-weight:500;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 30px;
+	font-family: "Overpass";
+	font-weight: 500;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   h3: `
-	font-size:24px;
-	font-family:"Poppins";
-	font-weight:500;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 26px;
+	font-family: "Overpass";
+	font-weight: 500;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
+`,
+  h4: `
+	font-size: 21px;
+	font-family: "Overpass";
+	font-weight: 500;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   bigParagraph: `
-	font-size:20px;
-	font-family:"Poppins";
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 20px;
+	font-family: "Open Sans";
+	font-weight: 400;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   bigParagraphMedium: `
-	font-size:20px;
-	font-family:"Poppins";
-	font-weight:500;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 20px;
+	font-family: "Open Sans";
+	font-weight: 600;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   bigParagraphSemibold: `
-	font-size:20px;
-	font-family:"Poppins";
-	font-weight:600;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 20px;
+	font-family: "Open Sans";
+	font-weight: 700;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   paragraph: `
-	font-size:16px;
-	font-family:"Poppins";
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 18px;
+	font-family: "Open Sans";
+	font-weight: 400;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   paragraphMedium: `
-	font-size:16px;
-	font-family:"Poppins";
-	font-weight:500;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 18px;
+	font-family: "Open Sans";
+	font-weight: 600;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   paragraphMediumItalic: `
-	font-size:16px;
-	font-family:"Poppins";
-	font-weight:500;
-	font-style:italic;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 18px;
+	font-family: "Poppins";
+	font-weight: 500;
+	font-style: italic;
+	text-decoration: none;
+	text-transform: none;
 `,
   paragraphSemibold: `
-	font-size:16px;
-	font-family:"Poppins";
-	font-weight:600;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 18px;
+	font-family: "Open Sans";
+	font-weight: 700;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   smallParagraph: `
-	font-size:14px;
-	font-family:"Poppins";
-	font-weight:400;
-	font-style:normal;
-	text-decoration:none;
-	text-transform:none;
+	font-size: 15px;
+	font-family: "Open Sans";
+	font-weight: 400;
+	font-style: normal;
+	text-decoration: none;
+	text-transform: none;
 `,
   landingPageH1: `
-	font-size:40px;
-	font-family:"Tapas";
-	text-decoration:none;
-	text-transform:none;
+	font-size: 40px;
+	font-family: "Tapas";
+	text-decoration: none;
+	text-transform: none;
 `,
   landingPageSmall: `
-	font-size:16px;
-	font-family:"Tapas";
-	text-decoration:none;
-	text-transform:none;
+	font-size: 16px;
+	font-family: "Tapas";
+	text-decoration: none;
+	text-transform: none;
 `,
 }
