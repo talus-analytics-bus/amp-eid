fabf0c53a57c78dfe16122c609f15c91b489d400
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-03-03 22:28:45 -0300

    Refactoring topicList to match design

--- src/components/landing/TopicList.tsx
+++ src/components/landing/TopicList.tsx
@@ -1,96 +1,118 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 import { Link } from 'gatsby'
 
-import CMS from '@talus-analytics/library.airtable-cms'
-
-import useIndexPageData from 'cmsHooks/useIndexPageData'
 import useTopics from 'queryHooks/useTopics'
+import simplifyForUrl from 'utilities/simplifyForUrl'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import BlueCircleIcon from 'components/ui/BlueCircleIcon'
 
 const TopicLinkList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
-  margin-top: 30px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   gap: 20px;
-
-  @media (max-width: 800px) {
-    grid-template-columns: 1fr;
-  }
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 `
 const Li = styled.li`
-  flex-basis: 30%;
   display: block;
-  border: 1px solid ${({ theme }) => theme.black};
-  border-radius: 5px;
   overflow: hidden;
+  flex-basis: 30%;
+  flex-grow: 1;
+
+  @media (max-width: 800px) {
+    flex-basis: 45%;
+  }
+  @media (max-width: 500px) {
+    flex-basis: 100%;
+  }
 `
 const ImageLink = styled(Link)`
-  display: block;
   position: relative;
-  background: white;
   transition: 150ms ease;
+  border-radius: 5px;
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  padding: 15px;
 
   &:hover {
-    background: ${({ theme }) => theme.lightGray};
+    background: ${({ theme }) => theme.ampEidDarkBlue};
+    transition: 250ms ease;
   }
 `
 const ComingSoonLinkPlaceholder = styled.span`
-  display: block;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5px;
 `
 const ImageLinkTextContainer = styled.div`
   ${({ theme }) => theme.bigParagraph};
-  position: absolute;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: ${({ theme }) => theme.black};
-  background: rgba(255, 255, 255, 0.85);
+  color: ${({ theme }) => theme.white};
   border-radius: 5px;
+  text-align: center;
+  margin-bottom: 15px;
 `
 
+type Topic = Exclude<Queries.TopicsQuery['topics'], null>['nodes'][number]
+
+const getNameAndImage = (topic: Topic) => {
+  const topicName = topic?.data?.Topic
+  if (!topicName) throw new Error('All topics must have a name')
+
+  const image =
+    topic?.data?.Image?.localFiles?.at(0)?.childImageSharp?.gatsbyImageData
+  if (!image) throw new Error('All topics must have a map image')
+
+  return { topicName, image }
+}
+
+const TopicLink = ({ topic }: { topic: Topic }) => {
+  const { topicName, image } = getNameAndImage(topic)
+  return (
+    <Li>
+      <ImageLink to={`/topics/${simplifyForUrl(topicName)}`}>
+        <BlueCircleIcon name={topicName} size={40} />
+        <ImageLinkTextContainer>{topicName}</ImageLinkTextContainer>
+        <GatsbyImage image={image} alt={`${topicName} preview map`} />
+      </ImageLink>
+    </Li>
+  )
+}
+
+const DisabledTopicLink = ({ topic }: { topic: Topic }) => {
+  const theme = useTheme()
+  const { topicName, image } = getNameAndImage(topic)
+  return (
+    <Li>
+      <ComingSoonLinkPlaceholder>
+        <BlueCircleIcon name={topicName} size={40} bgColor={theme.darkGray} />
+        <ImageLinkTextContainer>{topicName}</ImageLinkTextContainer>
+        <GatsbyImage image={image} alt={`${topicName} map coming soon`} />
+      </ComingSoonLinkPlaceholder>
+    </Li>
+  )
+}
+
 const TopicList = () => {
-  const cmsData = useIndexPageData()
   const topics = useTopics()
 
   return (
     <TopicLinkList>
-      <Li>
-        <ImageLink to="/topics/trade-and-intellectual-property/">
-          <CMS.Image name="Topic 1 image" data={cmsData} />
-          <ImageLinkTextContainer>
-            {topics[0].data?.Topic}
-          </ImageLinkTextContainer>
-        </ImageLink>
-      </Li>
-      <Li>
-        <ComingSoonLinkPlaceholder>
-          <CMS.Image name="Topic 2 image" data={cmsData} />
-          <ImageLinkTextContainer>
-            <CMS.Text name="Topic 2 text" data={cmsData} />
-          </ImageLinkTextContainer>
-        </ComingSoonLinkPlaceholder>
-      </Li>
-      <Li>
-        <ComingSoonLinkPlaceholder>
-          <CMS.Image name="Topic 3 image" data={cmsData} />
-          <ImageLinkTextContainer>
-            <CMS.Text name="Topic 3 text" data={cmsData} />
-          </ImageLinkTextContainer>
-        </ComingSoonLinkPlaceholder>
-      </Li>
-      <Li>
-        <ComingSoonLinkPlaceholder>
-          <CMS.Image name="Topic 4 image" data={cmsData} />
-          <ImageLinkTextContainer>
-            <CMS.Text name="Topic 4 text" data={cmsData} />
-          </ImageLinkTextContainer>
-        </ComingSoonLinkPlaceholder>
-      </Li>
+      {topics.map(topic =>
+        topic?.data?.Disable ? (
+          <DisabledTopicLink key={topic?.data.Topic} topic={topic} />
+        ) : (
+          <TopicLink key={topic?.data?.Topic} topic={topic} />
+        )
+      )}
     </TopicLinkList>
   )
 }
