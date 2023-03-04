d544839c0282253cce9c68dece9d9270e22ae6ba
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-03-03 21:41:51 -0300

    Making the blue background circle hideable

--- src/components/ui/BlueCircleIcon.tsx
+++ src/components/ui/BlueCircleIcon.tsx
@@ -2,23 +2,26 @@ import CMS, { CMSIconProps } from '@talus-analytics/library.airtable-cms'
 import React from 'react'
 import styled, { useTheme } from 'styled-components'
 
-const Container = styled.div`
+const Container = styled.div<{ hideBG: boolean | undefined }>`
   border-radius: 50%;
-  // padding: calc(0.5% + 1px);
+
+  background-color: ${({ theme, hideBG }) => !hideBG && theme.option1Lighter};
+  padding: ${({ hideBG }) => !hideBG && `calc(0.5% + 1px)`};
   flex-shrink: 0;
-  // position: relative;
-  // top: -0.04em;
+  position: relative;
+  top: ${({ hideBG }) => !hideBG && `-0.04em`};
 `
 
 interface BlueCircleIconProps extends CMSIconProps {
   size: number
+  hideBG?: boolean
 }
 
 const BlueCircleIcon = (props: BlueCircleIconProps) => {
-  const { size, ...iconProps } = props
+  const { size, hideBG, ...iconProps } = props
   const theme = useTheme()
   return (
-    <Container style={{ width: size, height: size }}>
+    <Container hideBG={hideBG} style={{ width: size, height: size }}>
       <CMS.Icon color={theme.black} {...iconProps} />
     </Container>
   )
