3dbe1c3f5abcc19cc64d014b1e6277756100edac
ryan-talus <rzimmerman@talusanalytics.com>
2023-04-04 22:44:37 -0300

    Reducing specificity slightly to allow for layout divs inside the header

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 
 const MainHeader = styled.header`
-  > h2 {
+  h2 {
     ${({ theme }) => theme.paragraph};
     color: ${({ theme }) => theme.darkGray};
     margin: 0;
   }
 
-  > h1 {
+  h1 {
     ${({ theme }) => theme.h1};
     color: ${({ theme }) => theme.ampEidDarkBlue};
     margin: 0;
