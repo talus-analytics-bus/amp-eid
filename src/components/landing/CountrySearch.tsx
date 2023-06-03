73c22ab89e573207a708ec36d3cecbcab7e5d666
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-03 19:02:32 +0000

    Stripping out all the rest of the nav-specific code from the landing page search component

--- src/components/landing/CountrySearch.tsx
+++ src/components/landing/CountrySearch.tsx
@@ -19,17 +19,9 @@ const Container = styled.div`
   margin: 0 15px;
 `
 
-enum SearchStatus {
-  Initial = 'initial',
-  Focused = 'focused',
-  Loading = 'loading',
-}
-
 const CountrySearch = ({ style }: CoutnrySearchProps) => {
   const countries = useCountryNames()
   const theme = useTheme()
-
-  const [searchStatus, setSearchStatus] = useState(SearchStatus.Initial)
 
   const searchItems = useMemo(
     () =>
@@ -47,24 +39,8 @@ const CountrySearch = ({ style }: CoutnrySearchProps) => {
     [countries]
   )
 
-  const lightMode = [SearchStatus.Loading, SearchStatus.Focused].includes(
-    searchStatus
-  )
-  const iconColor = lightMode ? '192C3C' : 'ffffff'
-
   return (
-    <Container
-      onFocus={() => setSearchStatus(SearchStatus.Focused)}
-      onBlur={() =>
-        setTimeout(
-          () =>
-            setSearchStatus(prev =>
-              prev === SearchStatus.Focused ? SearchStatus.Initial : prev
-            ),
-          150
-        )
-      }
-    >
+    <Container>
       <Typeahead
         iconLeft
         multiselect
@@ -74,11 +50,8 @@ const CountrySearch = ({ style }: CoutnrySearchProps) => {
         fontColor={theme.black}
         items={searchItems}
         placeholder={`Search for a country`}
-        onAdd={item => {
-          setSearchStatus(SearchStatus.Loading)
-          navigate(`/countries/${simplifyForUrl(item.label)}`)
-        }}
-        iconSVG={`%3Csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.01 11.255H12.22L11.94 10.985C12.92 9.845 13.51 8.365 13.51 6.755C13.51 3.165 10.6 0.254997 7.01001 0.254997C3.42001 0.254997 0.51001 3.165 0.51001 6.755C0.51001 10.345 3.42001 13.255 7.01001 13.255C8.62001 13.255 10.1 12.665 11.24 11.685L11.51 11.965V12.755L16.51 17.745L18 16.255L13.01 11.255ZM7.01001 11.255C4.52001 11.255 2.51001 9.245 2.51001 6.755C2.51001 4.265 4.52001 2.255 7.01001 2.255C9.50001 2.255 11.51 4.265 11.51 6.755C11.51 9.245 9.50001 11.255 7.01001 11.255Z' fill='%23${iconColor}'/%3E%3C/svg%3E%0A`}
+        onAdd={item => navigate(`/countries/${simplifyForUrl(item.label)}`)}
+        iconSVG={`%3Csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.01 11.255H12.22L11.94 10.985C12.92 9.845 13.51 8.365 13.51 6.755C13.51 3.165 10.6 0.254997 7.01001 0.254997C3.42001 0.254997 0.51001 3.165 0.51001 6.755C0.51001 10.345 3.42001 13.255 7.01001 13.255C8.62001 13.255 10.1 12.665 11.24 11.685L11.51 11.965V12.755L16.51 17.745L18 16.255L13.01 11.255ZM7.01001 11.255C4.52001 11.255 2.51001 9.245 2.51001 6.755C2.51001 4.265 4.52001 2.255 7.01001 2.255C9.50001 2.255 11.51 4.265 11.51 6.755C11.51 9.245 9.50001 11.255 7.01001 11.255Z' fill='%23192C3C'/%3E%3C/svg%3E%0A`}
       />
     </Container>
   )
