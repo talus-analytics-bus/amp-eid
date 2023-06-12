18938e86615cfd7be7e01bf0e967ef74a09f5b77
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-12 14:01:28 +0000

    Making the csvdownloadlink use the buttonlink button

--- src/components/ui/CSVDownloadLink.tsx
+++ src/components/ui/CSVDownloadLink.tsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react'
 import Papa from 'papaparse'
+import ButtonLink from './ButtonLink'
 
 interface CSVDownloadLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   csvData: { [key: string]: string }[]
@@ -10,9 +11,9 @@ const CSVDownloadLink = memo(({ csvData, ...props }: CSVDownloadLinkProps) => {
   const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
 
   return (
-    <a href={URL.createObjectURL(blob)} download="data.csv" {...props}>
+    <ButtonLink href={URL.createObjectURL(blob)} download="data.csv" {...props}>
       Download CSV ({Math.round(blob.size / 1024)} kb)
-    </a>
+    </ButtonLink>
   )
 })
 
