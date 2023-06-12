c379908305600b224b6b62f8c67aebc8633403a9
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-12 19:17:45 +0000

    Adding download icon to csv download button

--- src/components/ui/CSVDownloadLink.tsx
+++ src/components/ui/CSVDownloadLink.tsx
@@ -8,6 +8,22 @@ interface CSVDownloadLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   data: { [key: string]: string }[]
 }
 
+const DownloadIcon = () => (
+  <svg
+    width="12"
+    height="13"
+    viewBox="0 0 12 13"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ marginRight: '0.5rem' }}
+  >
+    <path
+      d="M11.25 4.75H8.25V0.25H3.75V4.75H0.75L6 10L11.25 4.75ZM0.75 11.5V13H11.25V11.5H0.75Z"
+      fill="white"
+    />
+  </svg>
+)
+
 const CSVDownloadLink = memo(
   ({ filename, label, data, ...props }: CSVDownloadLinkProps) => {
     const csv = Papa.unparse(data)
@@ -19,6 +35,7 @@ const CSVDownloadLink = memo(
         download={filename + '.csv'}
         {...props}
       >
+        <DownloadIcon />
         {label} (CSV, {Math.round(blob.size / 1024)} kb)
       </ButtonLink>
     )
