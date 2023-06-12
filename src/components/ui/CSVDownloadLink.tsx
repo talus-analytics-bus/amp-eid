395409c539e4d2a258315738c3bd5857c31f4174
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-12 13:56:25 +0000

    Cleaning up how the csv download component works

--- src/components/ui/CSVDownloadLink.tsx
+++ src/components/ui/CSVDownloadLink.tsx
@@ -7,14 +7,11 @@ interface CSVDownloadLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
 
 const CSVDownloadLink = memo(({ csvData, ...props }: CSVDownloadLinkProps) => {
   const csv = Papa.unparse(csvData)
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
 
   return (
-    <a
-      href={`data:text/csv;charset=utf-8,${encodeURIComponent(csv)}`}
-      download="data.csv"
-      {...props}
-    >
-      Download CSV
+    <a href={URL.createObjectURL(blob)} download="data.csv" {...props}>
+      Download CSV ({Math.round(blob.size / 1024)} kb)
     </a>
   )
 })
