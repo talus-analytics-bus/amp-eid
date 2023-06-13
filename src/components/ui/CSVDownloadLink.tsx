1cf8463617e63fb44f8b7402bc781f9f03aee6a6
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-13 11:44:28 +0000

    Refactoring download icon into separate component

--- src/components/ui/CSVDownloadLink.tsx
+++ src/components/ui/CSVDownloadLink.tsx
@@ -1,28 +1,13 @@
 import React, { memo } from 'react'
 import Papa from 'papaparse'
 import ButtonLink from './ButtonLink'
+import DownloadIcon from './DownloadIcon'
 
 interface CSVDownloadLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   filename: string
   label: React.ReactNode
   data: { [key: string]: string }[]
 }
-
-const DownloadIcon = () => (
-  <svg
-    width="12"
-    height="13"
-    viewBox="0 0 12 13"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    style={{ marginRight: '0.5rem' }}
-  >
-    <path
-      d="M11.25 4.75H8.25V0.25H3.75V4.75H0.75L6 10L11.25 4.75ZM0.75 11.5V13H11.25V11.5H0.75Z"
-      fill="white"
-    />
-  </svg>
-)
 
 const CSVDownloadLink = memo(
   ({ filename, label, data, ...props }: CSVDownloadLinkProps) => {
---
+++ src/components/ui/DownloadIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DownloadIcon = () => (
+  <svg
+    width="12"
+    height="13"
+    viewBox="0 0 12 13"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{ marginRight: '0.5rem' }}
+  >
+    <path
+      d="M11.25 4.75H8.25V0.25H3.75V4.75H0.75L6 10L11.25 4.75ZM0.75 11.5V13H11.25V11.5H0.75Z"
+      fill="white"
+    />
+  </svg>
+)
+
+export default DownloadIcon
