d648115607d47d4e02a36b63118270e7ed1bc511
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-06-13 12:08:26 +0000

    Fixing typescript in the API schema documentation; it helped me find two with missing definitions

--- src/pages/about/api.tsx
+++ src/pages/about/api.tsx
@@ -93,67 +93,73 @@ const StyledLink = styled.a`
   color: ${({ theme }) => theme.link};
 `
 
+enum SchemaObjectTypes {
+  Object,
+  Array,
+  String,
+}
+
 const schema = {
-  type: 'object',
+  type: SchemaObjectTypes.Object as const,
   definition: 'All data',
   properties: {
     Topics: {
-      type: 'array',
+      type: SchemaObjectTypes.Array as const,
       definition: 'List of topics',
       items: {
-        type: 'object',
+        type: SchemaObjectTypes.Object as const,
         definition: 'Topic object',
         properties: {
           Name: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Name of the topic',
           },
           Method: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Method description of the topic',
           },
           Subtopics: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'List of subtopics',
             items: {
-              type: 'object',
+              type: SchemaObjectTypes.Object as const,
               definition: 'Subtopic object',
               properties: {
                 Name: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Name of the subtopic',
                 },
                 Description: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Description of the subtopic',
                 },
                 Source: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Source annotation of the subtopic',
                 },
                 Statuses: {
-                  type: 'array',
+                  type: SchemaObjectTypes.Array as const,
                   definition:
                     'Categories grouping each country relative to the subtopic',
                   items: {
-                    type: 'object',
+                    type: SchemaObjectTypes.Object as const,
                     definition: 'Status object',
                     properties: {
                       Name: {
-                        type: 'string',
+                        type: SchemaObjectTypes.String as const,
                         definition:
                           'Name of the status, the same as the key on the map',
                       },
                       Description: {
-                        type: 'string',
+                        type: SchemaObjectTypes.String as const,
                         definition:
                           'Description of the status relative to the subtopic',
                       },
                       Countries: {
-                        type: 'array',
+                        type: SchemaObjectTypes.Array as const,
                         definition: 'List of countries with the status',
                         items: {
-                          type: 'string',
+                          type: SchemaObjectTypes.String as const,
                           definition: 'ISO3 code of the country',
                         },
                       },
@@ -167,86 +173,86 @@ const schema = {
       },
     },
     Countries: {
-      type: 'array',
+      type: SchemaObjectTypes.Array as const,
       definition: 'List of countries with their treaties and documents',
       items: {
-        type: 'object',
+        type: SchemaObjectTypes.Object as const,
         definition: 'Country object',
         properties: {
           ISO3: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'ISO3 code of the country',
           },
           Name: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Name of the country as displayed on the site',
           },
           Treaties: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition:
               "List of treaties on the site with the country's status and key dates",
             items: {
-              type: 'object',
+              type: SchemaObjectTypes.Object as const,
               definition: 'Country-treaty object',
               properties: {
                 Name: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Name of the treaty',
                 },
                 Status: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Status of the country relative to the treaty',
                 },
                 Date_signed: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Date the country signed the treaty',
                 },
                 Date_ratified: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Date the country ratified the treaty',
                 },
                 Date_entered_into_force: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Date the treaty entered into force',
                 },
               },
             },
           },
           Documents: {
-            type: 'object',
+            type: SchemaObjectTypes.Object as const,
             definition: 'List of documents applicable to the country',
             properties: {
               Name: {
-                type: 'string',
+                type: SchemaObjectTypes.String as const,
                 definition: 'Name of the document',
               },
               Type: {
-                type: 'string',
+                type: SchemaObjectTypes.String as const,
                 definition:
                   'Type of document, one of "Treaty", "Law", "Regulation", "Supporting document", or "Other"',
               },
               Date_of_original_publication: {
-                type: 'string',
+                type: SchemaObjectTypes.String as const,
                 definition: 'Date the document was originally published',
               },
               Relevant_articles: {
-                type: 'string',
+                type: SchemaObjectTypes.String as const,
                 definition:
                   "Relevant document section or articles for determining the country's status in the appropriate topic",
               },
               Topics: {
-                type: 'array',
+                type: SchemaObjectTypes.Array as const,
                 definition: 'List of topics where the document is relevant',
                 items: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Name of the topic',
                 },
               },
               Subtopics: {
-                type: 'array',
+                type: SchemaObjectTypes.Array as const,
                 definition: 'List of subtopics where the document is relevant',
                 items: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Name of the subtopic',
                 },
               },
@@ -256,100 +262,102 @@ const schema = {
       },
     },
     Treaties: {
-      type: 'array',
+      type: SchemaObjectTypes.Array as const,
       definition: 'List of treaties with their states parties and key dates',
       items: {
-        type: 'object',
+        type: SchemaObjectTypes.Object as const,
         definition: 'Treaty object',
         properties: {
           Name: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Name of the treaty',
           },
           Short_name: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Short name of the treaty, used in the URL and menus',
           },
           Description: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Description of the treaty',
           },
           Treaty_footnotes: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition:
               'Footnotes for the treaty, such as membership notes or process',
           },
           Date_of_original_publication: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Date the treaty was originally published',
           },
           Languages: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition:
               'List of languages the treaty is available in on the site',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
               definition: 'Language name of the treaty',
             },
           },
           Original_language_titles: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition:
               'List of original language titles of the treaty, order matches the Languages array',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
+              definition: 'Title of the treaty in its original language,',
             },
           },
           Related_treaties: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'List of related treaties, if any',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
+              definition: 'Name of the related treaty',
             },
           },
           States_parties: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'List of states parties to the treaty',
             items: {
-              type: 'object',
+              type: SchemaObjectTypes.Object as const,
               definition: 'State party object',
               properties: {
                 ISO3: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'ISO3 code of the country',
                 },
                 Name: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Name of the country',
                 },
                 Status: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Status of the country relative to the treaty',
                 },
                 Date_entered_into_force: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition:
                     'Date the treaty entered into force in the given country',
                 },
                 Date_ratified: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Date the country ratified the treaty',
                 },
                 Date_signed: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Date the country signed the treaty',
                 },
                 Reservations_understandings_and_declarations: {
-                  type: 'array',
+                  type: SchemaObjectTypes.Array as const,
                   definition:
                     'Reservations, understandings and declarations, if any',
                   items: {
-                    type: 'string',
+                    type: SchemaObjectTypes.String as const,
                     definition: 'Type of RUDs which apply to the country',
                   },
                 },
                 RUDs_text: {
-                  type: 'string',
+                  type: SchemaObjectTypes.String as const,
                   definition: 'Text of the RUDs',
                 },
               },
@@ -359,83 +367,83 @@ const schema = {
       },
     },
     Documents: {
-      type: 'array',
+      type: SchemaObjectTypes.Array as const,
       definition: 'List of documents available on the site, excluding Treaties',
       items: {
-        type: 'object',
+        type: SchemaObjectTypes.Object as const,
         definition: 'Document object',
         properties: {
           Name: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Name of the document',
           },
           Subtitle: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Document subtitle',
           },
           Authoring_country: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Name of the country that authored the document',
           },
           Authoring_regional_organization: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition:
               'Name of the regional organization that authored the document, e.g. "European union"',
           },
           All_applicable_countries: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'List of all countries where the document applies',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
               definition: 'ISO3 of the country',
             },
           },
           Topics: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'Topics which reference this document',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
               definition: 'Name of the topic',
             },
           },
           Subtopics: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'Subtopics which reference this document',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
               definition: 'Name of the subtopic',
             },
           },
           Document_type: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition:
               'Type of document, one of "Treaty", "Law", "Regulation", "Supporting document", or "Other"',
           },
           Description: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Description of the document',
           },
           Date_of_original_publication: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Date the document was originally published',
           },
           Date_entered_into_force: {
-            type: 'string',
+            type: SchemaObjectTypes.String as const,
             definition: 'Date the document entered into force',
           },
           Languages: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'List of available languages',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
               definition: 'Language name',
             },
           },
           Original_language_titles: {
-            type: 'array',
+            type: SchemaObjectTypes.Array as const,
             definition: 'Original language titles of the document',
             items: {
-              type: 'string',
+              type: SchemaObjectTypes.String as const,
               definition:
                 'Original language title, order matches Languages array',
             },
@@ -447,13 +455,13 @@ const schema = {
 }
 
 interface SchemaArray {
-  type: 'array'
+  type: SchemaObjectTypes.Array
   definition: string
   items: SchemaObject | SchemaArray | SchemaString
 }
 
 interface SchemaObject {
-  type: 'object'
+  type: SchemaObjectTypes.Object
   definition: string
   properties: {
     [key: string]: SchemaObject | SchemaArray | SchemaString
@@ -461,7 +469,7 @@ interface SchemaObject {
 }
 
 interface SchemaString {
-  type: 'string'
+  type: SchemaObjectTypes.String
   definition: string
 }
 
@@ -505,11 +513,11 @@ const RenderChild = ({
   data: SchemaArray | SchemaObject | SchemaString
 }) => {
   switch (data.type) {
-    case 'array':
+    case SchemaObjectTypes.Array:
       return <RenderArray {...{ name, data }} />
-    case 'object':
+    case SchemaObjectTypes.Object:
       return <RenderObject {...{ data }} />
-    case 'string':
+    case SchemaObjectTypes.String:
       return <RenderString {...{ data }} />
   }
 }
